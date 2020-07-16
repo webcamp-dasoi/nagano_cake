938ce6776c8a2492f8cd5b4143c4eff9d80d1d61
vagrant <vagrant@localhost.localdomain>
2020-07-16 12:35:43 +0000

    create addresses_index page

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,2 +1,29 @@
 class AddressesController < ApplicationController
+	
+  def index
+    @addresses = Address.all
+   	 @address = Address.new
+  end
+
+  def new
+  	 @address = Address.new
+  end
+
+  def create
+  	 @address = Address.new(book_params)
+    @address.end_user_id = current_end_user.id
+    if @address.save
+      redirect_to @book
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:post_number, :address,:name)
+  end
+
 end
