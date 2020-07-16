56d057a52b5f1f62e8058b3cdf678dbe235796f7
vagrant <vagrant@localhost.localdomain>
2020-07-16 03:13:41 +0000

    Change Address model content

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,6 @@ class Address < ApplicationRecord
 
 	belongs_to :end_user
 
-	validates :post_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+	validates :post_number, format: {with: /\A\d{3}[-]\d{4}\z/ }
 
 end
