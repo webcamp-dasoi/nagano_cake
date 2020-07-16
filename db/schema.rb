79673ada50b140d8626e2c95c76ef94df06686f2
vagrant <vagrant@localhost.localdomain>
2020-07-16 08:42:08 +0000

    rollback database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_16_061601) do
+ActiveRecord::Schema.define(version: 2020_07_16_044649) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "name"
@@ -33,14 +33,6 @@ ActiveRecord::Schema.define(version: 2020_07_16_061601) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
-  create_table "cart_products", force: :cascade do |t|
-    t.integer "quantity"
-    t.integer "product_id"
-    t.integer "end_user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "end_users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -49,6 +41,14 @@ ActiveRecord::Schema.define(version: 2020_07_16_061601) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "last_name"
+    t.string "first_name"
+    t.string "kana_last_name"
+    t.string "kana_first_name"
+    t.string "post_number"
+    t.string "address"
+    t.string "telephone_number"
+    t.boolean "is_active"
     t.index ["email"], name: "index_end_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
   end
