80dfbda3c09d3f07456873110faf878e4f0bd513
ichiha-off <bsk.6510riku@gmail.com>
2020-07-16 05:45:56 +0000

    Add addresses To edit,update

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,2 +1,21 @@
 class AddressesController < ApplicationController
+
+  def edit 
+    @addresses = Addresses.find(params[:id])
+  end
+
+  def update
+    @addresses = Addresses.find(params[:id])
+    @addresses.save(addresses_params)
+  end
+
+  private
+
+  def addresses_params
+  	params.require(:address).permit(
+    :name,
+    :post_number,
+    :address )
+  end
+
 end
