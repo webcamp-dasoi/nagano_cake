0e2ab9f646544aab61516512426b4c1926cbcd33
ichiha-off <bsk.6510riku@gmail.com>
2020-07-18 03:33:23 +0000

    FixOrderStatus

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
   
   belongs_to :end_user
 
-  enum order_status: {"カード": 0, "現金": 1}
+  enum order_status: {"クレジットカード": 0, "銀行振込": 1}
 
   enum payment_method: {"入金待ち": 0, "入金確認": 1, "制作中": 2, "発送準備中": 3, "発送済み": 4}
 
