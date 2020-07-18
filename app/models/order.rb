f29b261b6194207e1b29da601eea24758e4c06b6
ichiha-off <bsk.6510riku@gmail.com>
2020-07-18 02:51:57 +0000

    Add OrderModel

---
+++ app/models/order.rb
@@ -0,0 +1,11 @@
+class Order < ApplicationRecord
+  
+  belongs_to :end_user
+
+  enum order_status: {"カード": 0, "現金": 1}
+
+  enum payment_method: {"入金待ち": 0, "入金確認": 1, "制作中": 2, "発送準備中": 3, "発送済み": 4}
+
+  validates :shipping_post_number, format: {with: /\A\d{3}[-]\d{4}\z/ }
+
+end
