4f52e97c9b660f444a9de389b501a5b4e2d6bd64
vagrant <vagrant@localhost.localdomain>
2020-07-18 05:49:19 +0000

    create order_products_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_16_061601) do
+ActiveRecord::Schema.define(version: 2020_07_18_054319) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "name"
@@ -59,6 +59,16 @@ ActiveRecord::Schema.define(version: 2020_07_16_061601) do
     t.boolean "is_active"
     t.index ["email"], name: "index_end_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
+  end
+
+  create_table "order_products", force: :cascade do |t|
+    t.integer "quantity"
+    t.integer "tax_price"
+    t.integer "producing_status"
+    t.integer "product_id"
+    t.integer "order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
