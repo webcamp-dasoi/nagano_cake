1c3b84ac81013f37c810f89ae4f08794b4e64120
vagrant <vagrant@localhost.localdomain>
2020-07-18 05:14:47 +0000

    Add addresses_create

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,2 +1,11 @@
 class AddressesController < ApplicationController
+	def create
+		@address = Address.new(address_params)
+		@address.end_user_id = current_user.id
+		@address.save
+		redirect_to addresses_path
+
+	private
+	def address_params
+		params.require(:address).permit(:name, :post_number, :address)
 end
