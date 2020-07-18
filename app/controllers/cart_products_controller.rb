b011bebce51ca5c94ae6eb97644de772a525c025
vagrant <vagrant@localhost.localdomain>
2020-07-18 09:38:53 +0000

    create cart_products_update_page

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -1,2 +1,10 @@
 class CartProductsController < ApplicationController
+def update
+	@cart_prpduct = CartProduct.find(params[:id])
+    if @cart_product.update(cart_product_params)
+       redirect_to ''
+    else
+       render ''
+    end
+end
 end
