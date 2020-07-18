f91d8f760e3fae094c327db74aeb316dff347df2
ichiha-off <bsk.6510riku@gmail.com>
2020-07-18 04:49:21 +0000

    FixOrderModel

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,9 +2,9 @@ class Order < ApplicationRecord
   
   belongs_to :end_user
 
-  enum order_status: {"クレジットカード": 0, "銀行振込": 1}
+  enum payment_method: {"クレジットカード": 0, "銀行振込": 1}
 
-  enum payment_method: {"入金待ち": 0, "入金確認": 1, "制作中": 2, "発送準備中": 3, "発送済み": 4}
+  enum order_status: {"入金待ち": 0, "入金確認": 1, "製作中": 2, "発送準備中": 3, "発送済み": 4}
 
   validates :shipping_post_number, format: {with: /\A\d{3}[-]\d{4}\z/ }
 
