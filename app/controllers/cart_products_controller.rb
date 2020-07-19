8454e3415fe23020b1f33f757d12f5f5d8234ae2
vagrant <vagrant@localhost.localdomain>
2020-07-19 07:53:07 +0000

    create cart_product_destroy

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -1,2 +1,7 @@
 class CartProductsController < ApplicationController
+
+def destroy
+	@cart_product = CartProducts.find(params[:id])
+    @cart_product.destroy
+    redirect_to cart_products_path
 end
