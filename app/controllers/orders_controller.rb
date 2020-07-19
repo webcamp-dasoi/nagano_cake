cd935165b5aa12e37be21573f03c4ccb161a4245
ichiha-off <bsk.6510riku@gmail.com>
2020-07-19 09:29:20 +0000

    AddOrdersToNew

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,21 @@
 class OrdersController < ApplicationController
+
+  def new
+    @order = Order.new
+    @address = current_end_user.addresses
+  end
+
+  def confirm
+  end
+
+
+  private
+
+  def order_params
+  	params.require(:order).permit(
+      :payment_method,
+      :shipping_name,
+      :shipping_post_number,
+      :shipping_address,)
+  end
 end
