aed5daef81d78f50ab288077486cf97fb789826c
vagrant <vagrant@localhost.localdomain>
2020-07-19 09:43:45 +0000

    cart_products_update

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -2,9 +2,9 @@ class CartProductsController < ApplicationController
 def update
 	@cart_prpduct = CartProduct.find(params[:id])
     if @cart_product.update(cart_product_params)
-       redirect_to ''
+       redirect_to cart_products_path
     else
-       render ''
+       redirect_to @cart_prpduct
     end
 end
 end
