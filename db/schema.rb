d6bd396a1b784a32f8492a08c5156047042f0c48
ichiha-off <bsk.6510riku@gmail.com>
2020-07-20 06:24:59 +0000

    AddGenresModel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_18_054319) do
+ActiveRecord::Schema.define(version: 2020_07_20_061624) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "name"
@@ -56,9 +56,16 @@ ActiveRecord::Schema.define(version: 2020_07_18_054319) do
     t.string "post_number"
     t.string "address"
     t.string "telephone_number"
-    t.boolean "is_active", default: true
+    t.boolean "is_active"
     t.index ["email"], name: "index_end_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "order_products", force: :cascade do |t|
