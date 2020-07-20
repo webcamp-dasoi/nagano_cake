fcc33a68eef6dc6c381739138f7dc569fbae00fc
vagrant <vagrant@localhost.localdomain>
2020-07-20 02:04:00 +0000

    update addresses_index

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,28 +1,23 @@
 class AddressesController < ApplicationController
-
 def index
-    @addresses = Address.find(params[:id])
-   	@address = Address.new
-end
-
-def new
-  	@address = Address.new
+   @addresses = @end_user.ddresses
 end
 
 def create
-  	@address = Address.new(address_params)
+    @address = Address.new(address_params)
     @address.end_user_id = current_end_user.id
-    if @address.save
-       redirect_to @address
-    else
-       render 'index'
-    end
+  if @address.save
+     redirect_to addresses_path
+  else
+      @addresses = Address.find(params[:id])
+      render :index
+  end
 end
 
-private
-
-def address_params
-    params.require(:address).permit(:post_number, :address, :name)
+def destroy
+ 	@address = Address.find(params[:id])
+    @address.destroy
+    redirect_to addresses_path
 end
 
 end
