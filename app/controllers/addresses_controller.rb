c88a2f9e7d18778ed01aa6261876e3b5ce6d5d3b
vagrant <vagrant@localhost.localdomain>
2020-07-20 02:52:19 +0000

    fix addresses_destroy

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,18 +1,4 @@
 class AddressesController < ApplicationController
-def index
-   @addresses = @end_user.ddresses
-end
-
-def create
-    @address = Address.new(address_params)
-    @address.end_user_id = current_end_user.id
-  if @address.save
-     redirect_to addresses_path
-  else
-      @addresses = Address.find(params[:id])
-      render :index
-  end
-end
 
 def destroy
  	@address = Address.find(params[:id])
