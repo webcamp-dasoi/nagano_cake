dad849a1736cb841abc719dec9ca8a23a1a91155
vagrant <vagrant@localhost.localdomain>
2020-07-20 13:03:08 +0000

    create owner_products_create

--- app/controllers/owner/products_controller.rb
+++ app/controllers/owner/products_controller.rb
@@ -5,4 +5,20 @@ class Owner::ProductsController < ApplicationController
 		@genres = Genre.all
 	end
 
+	def create
+		@product = Product.new(product_params)
+		if @product.save
+			redirect_to :show
+		else
+			@genres = Genre.all
+			render :new
+		end
+	end
+
+	private
+
+	def product_params
+		params.require(:product).permit(:name, :introduction, :non_tax_price, :image_id, :is_active, :genre_id)
+	end
+
 end
