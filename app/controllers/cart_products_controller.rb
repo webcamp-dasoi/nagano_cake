854dfbc7dcca15f216995917e577947d813f1455
Yuki Kanayama <64630476+ichiha-off@users.noreply.github.com>
2020-07-21 11:33:27 +0900

    Update cart_products_controller.rb

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -1,10 +1,12 @@
 class CartProductsController < ApplicationController
-def update
-	@cart_prpduct = CartProduct.find(params[:id])
-    if @cart_product.update(cart_product_params)
-       redirect_to cart_products_path
-    else
-       redirect_to @cart_prpduct
-    end
-end
+	
+	def update
+		@cart_prpduct = CartProduct.find(params[:id])
+	    if	@cart_product.update(cart_product_params)
+		redirect_to cart_products_path
+	    else
+		redirect_to cart_products_path
+	    end
+	end
+	
 end
