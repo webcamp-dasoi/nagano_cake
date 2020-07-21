a9d6362f56c99ba8f78eb6d9cebb0fa5f9ee8932
ichiha-off <bsk.6510riku@gmail.com>
2020-07-21 07:52:29 +0000

    AddOrdersToCreate

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,30 @@ class OrdersController < ApplicationController
     @end_user = current_end_user
   end
 
+  def create
+    if params[:shipping] == '0'
+      @order = Order.new(order_params)
+      @order.end_user_id = current_end_user.id
+      if @order.save
+        current_end_user.cart_products.destroy
+        redirect_to orders_finish_path
+      else
+        redirect_back(fallback_location: root_path)
+      end
+    elsif params[:shipping] == '1'
+      
+    elsif params[:shipping] == '2'
+      @order = Order.new(order_params)
+      @order.end_user_id = current_end_user.id
+      if @order.save
+        current_end_user.cart_products.destroy
+        redirect_to orders_finish_path
+      else
+        redirect_back(fallback_location: root_path)
+      end
+    end
+  end
+
   def finish
   end
 
@@ -25,3 +49,32 @@ class OrdersController < ApplicationController
   end
 
 end
+
+# if params[:order][:shipping] == '0'
+#   @order = params[:order][:shipping_name] == current_end_user.last_name, params[:order][:shipping_post_number] == current_end_user.shipping_post_number, params[:order][:shipping_address] == current_end_user.shipping_address
+#   if @order.save
+#     current_end_user.cart_products.destroy
+#     redirect_to orders_finish_path
+#   else
+#     redirect_back(fallback_location: root_path)
+#   end
+
+# elsif params[:order][:shipping] == '1'
+#   if @order.save
+#     current_end_user.cart_products.destroy
+#     redirect_to orders_finish_path
+#   else
+#     redirect_back(fallback_location: root_path)
+#   end
+
+# elsif params[:order][:shipping] == '2'
+#   @order = Order.new(order_params)
+#   @order.end_user_id = current_end_user.id
+#   if @order.save
+#     current_end_user.cart_products.destroy
+#     redirect_to orders_finish_path
+#   else
+#     redirect_back(fallback_location: root_path)
+#   end
+
+# end
