22b3103486827953cee51af098e01427d6ffd567
vagrant <vagrant@localhost.localdomain>
2020-07-23 05:56:14 +0000

    owner_order_edit

--- app/controllers/owner/orders_controller.rb
+++ app/controllers/owner/orders_controller.rb
@@ -1,2 +1,8 @@
 class Owner::OrdersController < ApplicationController
+
+	def edit
+		@order = Order.find(params[:id])
+		@order_product = OrderProduct.where(order_id: @order.id)
+		@end_user = @order.end_user
+	end
 end
