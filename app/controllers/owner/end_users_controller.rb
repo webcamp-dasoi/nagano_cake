c9e838dc21ef12c6c8668952ef94064ac4027090
vagrant <vagrant@localhost.localdomain>
2020-07-23 08:04:39 +0000

    repair owner_end_users_index

--- app/controllers/owner/end_users_controller.rb
+++ app/controllers/owner/end_users_controller.rb
@@ -1,7 +1,9 @@
 class Owner::EndUsersController < ApplicationController
 
 	def index
-		@end_user = EndUser.all
+		@end_users = EndUser.all
+	end
+
 	def show
 		@end_user = EndUser.find(params[:id])
 	end
