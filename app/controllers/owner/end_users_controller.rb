6320e7c6ec4c23a8597e4fad623e3d85af1b61e5
vagrant <vagrant@localhost.localdomain>
2020-07-23 07:18:14 +0000

    create owner_end_users_edit

--- app/controllers/owner/end_users_controller.rb
+++ app/controllers/owner/end_users_controller.rb
@@ -3,4 +3,29 @@ class Owner::EndUsersController < ApplicationController
 	def show
 		@end_user = EndUser.find(params[:id])
 	end
+
+	def edit
+		@end_user = EndUser.find(params[:id])
+	end
+
+	def update
+		@end_user = EndUser.find(params[:id])
+		if @end_user.update(end_user_params)
+        redirect_to edit_owner_end_user_path
+        else
+        render 'edit'
+        end
+	end
+
+	private
+  	def end_user_params
+  		params.require(:user).permit(
+  			:last_name, 
+  			:first_name, 
+  			:last_name_kana, 
+  			:first_name_kana, 
+  			:postal_number, 
+  			:telephone_number, 
+  			:email, 
+  			:is_active ? '有効':'無効')
 end
