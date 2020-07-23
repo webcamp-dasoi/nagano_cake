07eb45a6e2e0131793ad5b9497415904ff6ca5fc
ichiha-off <bsk.6510riku@gmail.com>
2020-07-23 07:39:19 +0000

    Fix

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,4 +4,8 @@ class Address < ApplicationRecord
 
 	validates :post_number, format: {with: /\A\d{3}[-]\d{4}\z/ }
 
+	def view_address
+		"#{self.post_number}　#{+ self.address}　#{+ self.name}"
+	end
+
 end
