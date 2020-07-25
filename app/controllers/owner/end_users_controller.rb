196d0e95e2a83cf52c33b43c98df9595cb56be83
Yuki Kanayama <64630476+ichiha-off@users.noreply.github.com>
2020-07-25 17:56:28 +0900

    Revert "create owner_end_users_edit"

--- app/controllers/owner/end_users_controller.rb
+++ app/controllers/owner/end_users_controller.rb
@@ -3,30 +3,4 @@ class Owner::EndUsersController < ApplicationController
 	def show
 		@end_user = EndUser.find(params[:id])
 	end
-
-	def edit
-		@end_user = EndUser.find(params[:id])
-	end
-
-	def update
-		   @end_user = EndUser.find(params[:id])
-		if @end_user.update(end_user_params)
-           redirect_to edit_owner_end_user_path
-        else
-           render 'edit'
-        end
-	end
-
-	private
-  	def end_user_params
-  		params.require(:user).permit(
-  			:last_name, 
-  			:first_name, 
-  			:kana_last_name, 
-  			:kana_first_nama, 
-  			:postal_number, 
-  			:telephone_number, 
-  			:email, 
-  			:is_active)
-  	end
 end
