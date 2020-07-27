d8233d5672f61c79815309175074c8fa2221551b
vagrant <vagrant@localhost.localdomain>
2020-07-27 11:34:01 +0000

    update owner_orders_edit

--- app/controllers/owner/order_products_controller.rb
+++ app/controllers/owner/order_products_controller.rb
@@ -3,8 +3,13 @@ class Owner::OrderProductsController < ApplicationController
   before_action :authenticate_admin!
 
 	def producing_update
+		@order = Order.find(params[:id])
 		@order_product = OrderProduct.find_by(id: params[:order_product][:id])
 		if @order_product.update(order_product_params)
+		   @order_products = OrderProduct.where(order_id: @order.id, producing_status: "製作完了")
+			if @order_products.to_a.count == OrderProduct.where(order_id: @order.id).to_a.count
+			   @order.update(order_status: "発送準備中")
+		   	end
 		   redirect_to edit_owner_order_path(@order_product.order)
 		else
 			render :edit
