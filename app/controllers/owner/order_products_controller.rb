72e8d58ae87fa18076b78be56badbf8bf4aaee81
vagrant <vagrant@localhost.localdomain>
2020-07-28 04:05:12 +0000

    fix_producing_update

--- app/controllers/owner/order_products_controller.rb
+++ app/controllers/owner/order_products_controller.rb
@@ -5,12 +5,21 @@ class Owner::OrderProductsController < ApplicationController
 	def producing_update
 		@order = Order.find(params[:id])
 		@order_product = OrderProduct.find_by(id: params[:order_product][:id])
-		if @order_product.update(order_product_params)
-		   @order_products = OrderProduct.where(order_id: @order.id, producing_status: "製作完了")
-			if @order_products.to_a.count == OrderProduct.where(order_id: @order.id).to_a.count
-			   @order.update(order_status: "発送準備中")
-		   	end
-		   redirect_to edit_owner_order_path(@order_product.order)
+	    if @order_product.update(order_product_params)
+	    	# 入力の値で分岐
+		    case params[:order_product][:producing_status]
+		    	# 製作ステータスが製作中の場合
+			   when params[:order_product][:producing_status] = "製作中"
+			   	    @order.update(order_status: "製作中")
+			   	# 製作ステータスが製作完了の場合
+			   when params[:order_product][:producing_status] = "製作完了"
+			   		@order_products = OrderProduct.where(order_id: @order.id)
+			        @statuses = OrderProduct.where(order_id: @order.id, producing_status: "製作完了")
+			   		if @order_products.to_a.count == @statuses.to_a.count
+				   	   @order.update(order_status: "発送準備中")
+				    end
+			end
+			redirect_to edit_owner_order_path(@order_product.order)
 		else
 			render :edit
 		end
@@ -21,6 +30,5 @@ class Owner::OrderProductsController < ApplicationController
 	def order_product_params
     	params.require(:order_product).permit(:quantity, :tax_price, :producing_status)
     end
-	before_action :authenticate_admin!
 	
 end
